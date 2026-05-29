672f83d6b4e62d6e4d8c7a439437c121fc1e0b8a
dhanywp20-cmyk <dhanywp20@gmail.com>
2026-05-29 17:47:30 +0700

    Update page.tsx

--- app/reminder-schedule/page.tsx
+++ app/reminder-schedule/page.tsx
@@ -2397,7 +2397,7 @@ jangan lupa peralatan & Semangat💪🏼
                           <col style={{ width: '7%' }} />
                           <col style={{ width: '9%' }} />
                           <col style={{ width: '6%' }} />
-                          <col style={{ width: '8%' }} />
+                          <col style={{ width: '5%' }} />
                         </colgroup>
                         <thead>
                           <tr className="border-b-2 border-gray-100" style={{ background: "rgba(255,255,255,0.97)" }}>
